bf7eda9e6cd468684a7e4a8c33a337d50b4918a3
Michal Moskal <michal@moskal.me>
2022-07-22 16:59:32 +0100

    use ping-pong DMA buffer for sound

--- inc/RP2040PWM.h
+++ inc/RP2040PWM.h
@@ -17,61 +17,59 @@ namespace codal
 class RP2040PWM : public CodalComponent, public DataSink
 {
 private:
-  uint8_t slice;
-  int sampleRate;
-  RP2040Pin *pin;
-  uint16_t playPos = 0;
-  uint16_t * buf0;
+    uint8_t slice;
+    int sampleRate;
+    RP2040Pin *pin;
+    uint16_t *buf[2];
+    int dmaCh[2];
 
 public:
-  bool playing;
-  // The stream component that is serving our data
-  DataSource &upstream;
-  ManagedBuffer output;
+    bool en;
+    // The stream component that is serving our data
+    DataSource &upstream;
+    ManagedBuffer output;
 
-  /**
-   * Constructor for an instance of a DAC.
-   *
-   * @param source The DataSource that will provide data.
-   * @param sampleRate The frequency (in Hz) that data will be presented.
-   * @param id The id to use for the message bus when transmitting events.
-   */
-  RP2040PWM(Pin &pin, DataSource &source, int sampleRate = RP2040PWM_DEFAULT_FREQUENCY,
-        uint16_t id = DEVICE_ID_SYSTEM_DAC);
+    /**
+     * Constructor for an instance of a DAC.
+     *
+     * @param source The DataSource that will provide data.
+     * @param sampleRate The frequency (in Hz) that data will be presented.
+     * @param id The id to use for the message bus when transmitting events.
+     */
+    RP2040PWM(Pin &pin, DataSource &source, int sampleRate = RP2040PWM_DEFAULT_FREQUENCY,
+              uint16_t id = DEVICE_ID_SYSTEM_DAC);
 
-  /**
-   * Callback provided when data is ready.
-   */
-  virtual int pullRequest();
+    /**
+     * Callback provided when data is ready.
+     */
+    virtual int pullRequest();
 
-  /**
-   * Determine the DAC playback sample rate to the given frequency.
-   * @return the current sample rate.
-   */
-  int getSampleRate();
+    /**
+     * Determine the DAC playback sample rate to the given frequency.
+     * @return the current sample rate.
+     */
+    int getSampleRate();
 
-  /**
-   * Change the DAC playback sample rate to the given frequency.
-   * @param frequency The new sample playback frequency.
-   */
-  int setSampleRate(int frequency);
+    /**
+     * Change the DAC playback sample rate to the given frequency.
+     * @param frequency The new sample playback frequency.
+     */
+    int setSampleRate(int frequency);
 
-  /**
-   * Enable this component
-   */
-  void enable();
+    /**
+     * Enable this component
+     */
+    void enable();
 
-  /**
-   * Disable this component
-   */
-  void disable();
+    /**
+     * Disable this component
+     */
+    void disable();
 
-  
-  void fillBuffer();
-  void irq(uint16_t slice_mask);
-
+    void dmaCB(int ch);
+    void dump();
 };
 
-}
+} // namespace codal
 
 #endif
--- source/RP2040PWM.cpp
+++ source/RP2040PWM.cpp
@@ -1,63 +1,113 @@
 #include "stdio.h"
 #include "RP2040PWM.h"
+#include "CodalDmesg.h"
 
 #include "hardware/clocks.h"
 #include "hardware/gpio.h"
 #include "hardware/irq.h"
 #include "hardware/pwm.h"
+#include "dma.h"
+#include "hardware/dma.h"
 
 namespace codal
 {
 
 // 10bit and 256 byte per chunk
 #define BIT_LEN (1 << 10)
-#define CHUNKSZ (256)
+#define CHUNKSZ (256 * 2)
 
-RP2040PWM *inst;
-
-extern "C" void isr_pwm_wrap()
+static void dma0_cb(void *inst)
 {
-    uint16_t n = pwm_hw->ints;
-    pwm_hw->intr = n;
-    inst->irq(n);
+    ((RP2040PWM *)inst)->dmaCB(0);
 }
-
-// TODO: add dual buffer for this
-void RP2040PWM::irq(uint16_t slice_mask)
+static void dma1_cb(void *inst)
 {
-    if (playPos < CHUNKSZ)
-    {
-        pwm_set_gpio_level(pin->name, buf0[playPos++]);
-        if (playPos == CHUNKSZ)
-        {
-            pwm_set_enabled(slice, false);
-            pwm_set_irq_enabled(slice, false);
-            inst->playing = false;
-            inst->fillBuffer();
-        }
-    }
+    ((RP2040PWM *)inst)->dmaCB(1);
 }
 
 RP2040PWM::RP2040PWM(Pin &pin, DataSource &source, int sampleRate, uint16_t id) : upstream(source)
 {
     this->pin = (RP2040Pin *)&pin;
-    this->playing = false;
-    buf0 = new uint16_t[CHUNKSZ];
+    this->en = false;
 
-    gpio_set_function(pin.name, GPIO_FUNC_PWM);
+    // gpio_set_drive_strength(pin.name, GPIO_DRIVE_STRENGTH_12MA);
+    pin.setDigitalValue(0);
 
     slice = pwm_gpio_to_slice_num(pin.name);
-    pwm_clear_irq(slice);
-    irq_set_enabled(PWM_IRQ_WRAP, true);
+    gpio_set_function(pin.name, GPIO_FUNC_PWM);
+
+    for (int i = 0; i < 2; ++i)
+    {
+        dmaCh[i] = dma_claim_unused_channel(true);
+        buf[i] = new uint16_t[CHUNKSZ / 2];
+    }
+
     setSampleRate(sampleRate);
-    inst = this;
     upstream.connect(*this);
+}
+
+void RP2040PWM::dump()
+{
+#if 0
+    uint32_t c0 = dma_get_channel_config(dmaCh[0]).ctrl;
+    uint32_t c1 = dma_get_channel_config(dmaCh[1]).ctrl;
+
+    DMESG("DMA ST %x/%d %x/%d", c0, (c0 >> 11) & 0xf, c1, (c1 >> 11) & 0xf);
+#endif
+}
+
+void RP2040PWM::dmaCB(int ch)
+{
+    dump();
+
+    // dmaCh[ch] finished - refill it's buffer
+    ManagedBuffer output = upstream.pull();
+    bool isMute = true;
+    if (output.length())
+    {
+        if (output.length() != CHUNKSZ)
+            target_panic(DEVICE_HARDWARE_CONFIGURATION_ERROR);
+
+        auto dp = (uint16_t *)output.getBytes();
+        for (int i = 0; i < 16; i++)
+        {
+            if (dp[i] != BIT_LEN / 2)
+            {
+                isMute = false;
+                break;
+            }
+        }
+    }
+
+    if (!isMute)
+    {
+        memcpy(buf[ch], output.getBytes(), CHUNKSZ);
+    }
+    else
+    {
+        memset(buf[ch], 0, CHUNKSZ);
+    }
+
+    volatile uint16_t *dst = (volatile uint16_t *)&pwm_hw->slice[slice].cc;
+    if (pin->name & 1)
+        dst++;
+
+    bool start = false;
+    dma_channel_config c = dma_channel_get_default_config(dmaCh[ch]);
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
+    channel_config_set_dreq(&c, DREQ_PWM_WRAP0 + slice);
+    channel_config_set_chain_to(&c, dmaCh[1 - ch]);
+    dma_channel_configure(dmaCh[ch], &c, dst, buf[ch], CHUNKSZ / 2, start);
+
+    dump();
+
+    DMA_SetChannelCallback(dmaCh[ch], ch == 0 ? dma0_cb : dma1_cb, this);
 }
 
 int RP2040PWM::setSampleRate(int frequency)
 {
     pwm_config config = pwm_get_default_config();
-    pwm_config_set_clkdiv(&config, 125.0 * MHZ / (frequency * BIT_LEN));
+    pwm_config_set_clkdiv(&config, (float)clock_get_hz(clk_sys) / (frequency * BIT_LEN));
     pwm_config_set_wrap(&config, BIT_LEN);
     pwm_init(slice, &config, false);
 
@@ -65,43 +115,10 @@ int RP2040PWM::setSampleRate(int frequency)
     return DEVICE_OK;
 }
 
-void RP2040PWM::fillBuffer()
-{
-    playing = 1;
-    output = upstream.pull();
-    if (output.length())
-    {
-        auto dp = (uint16_t *)output.getBytes();
-        bool isMute = true;
-        for (int i = 0; i < 16; i++)
-        {
-            if (dp[i] != 512)
-            {
-                isMute = false;
-                break;
-            }
-        }
-        // mute the channel but still keep the irq running.
-        if (!isMute)
-        {
-            memcpy(buf0, output.getBytes(), output.length());
-        }
-        else
-        {
-            memset(buf0, 0, output.length());
-        }
-        playPos = 0;
-        pwm_set_enabled(slice, true);
-        pwm_set_irq_enabled(slice, true);
-    }
-}
-
 int RP2040PWM::pullRequest()
 {
-    if (!playing)
-    {
-        fillBuffer();
-    }
+    // just enable; we keep pulling at our own pace anyways
+    enable();
     return DEVICE_OK;
 }
 
@@ -112,12 +129,29 @@ int RP2040PWM::getSampleRate()
 
 void RP2040PWM::enable()
 {
+    if (en)
+        return;
+    en = true;
+
+    // refill both buffers
+    dmaCB(0);
+    dmaCB(1);
+
     pwm_set_enabled(slice, true);
+    dma_channel_start(dmaCh[0]);
 }
 
 void RP2040PWM::disable()
 {
+    if (!en)
+        return;
+    en = false;
+
     pwm_set_enabled(slice, false);
+    dma_channel_abort(dmaCh[0]);
+    dma_channel_abort(dmaCh[1]);
+    pin->disconnect();
+    pin->setDigitalValue(0);
 }
 
 } // namespace codal
